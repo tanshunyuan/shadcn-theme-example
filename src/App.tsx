7e3a6ac353a5fb329bdaa08ee71edecbd05e18e6
Shun Yuan <tanshunyuan99@gmail.com>
2024-01-14 11:29:51 +0800

    chore(): add theme toggle

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
+import { ModeToggle } from "./components/mode-toggle";
+
 function App() {
   return (
     <>
+      <ModeToggle></ModeToggle>
       <p>whadug</p>
     </>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import './styles/globals.css'
-
+import "./styles/globals.css";
+import { ThemeProvider } from "./components/theme-provider.tsx";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
